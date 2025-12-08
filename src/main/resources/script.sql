8f4610eda5dc35dbce8c6b0419e7dbf64d6f7fe6
Dania Midera <daniamidera@gmail.com>
2025-12-09 01:44:36 +0400

    main: structure init

---
+++ src/main/resources/script.sql
@@ -0,0 +1,8 @@
+create table customer(
+    customer_id varchar(50),
+    customer_name varchar(100),
+    customer_mdp varchar(100),
+    customer_email varchar(100),
+    customer_tel varchar(100),
+    customer_dtn date
+);
